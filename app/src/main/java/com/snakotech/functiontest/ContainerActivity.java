02264ed1ac8e5bd483086f2a3f96252c0484b388
Zhangchuanchuan <543749092?@qq.com>
2017-08-12 16:31:29 +0800

    add fiel

---
+++ app/src/main/java/com/snakotech/functiontest/ContainerActivity.java
@@ -0,0 +1,11 @@
+package com.snakotech.functiontest;
+
+import android.app.Activity;
+
+/**
+ * Created by Administrator on 2017/8/12.
+ */
+
+public class ContainerActivity extends Activity {
+
+}
---
+++ app/src/main/java/com/snakotech/functiontest/LocalFunctionAPI.java
@@ -0,0 +1,10 @@
+package com.snakotech.functiontest;
+
+/**
+ * 本地方法的API
+ * Created by Administrator on 2017/8/12.
+ */
+
+public class LocalFunctionAPI {
+
+}
